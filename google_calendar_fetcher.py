a4182487cbc4bf273f1c03752e1aaff6f8eee180
chandai <guy1nthecha1r2049@gmail.com>
2023-06-22 00:36:31 +0900

    create poetry env

---
+++ google_calendar_fetcher.py
@@ -0,0 +1,9 @@
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# 各種 Google Calendar APIの依存関係 を import できたことを確認
+Request
+InstalledAppFlow
+build
