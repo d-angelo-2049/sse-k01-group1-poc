4d65bc931a5d058a6cd2bf7a85991c50b87c177b
chandai <guy1nthecha1r2049@gmail.com>
2023-06-23 21:18:16 +0900

    added google calendar event fetcher for poc

--- google_calendar_fetcher.py
+++ google_calendar_fetcher.py
@@ -1,9 +1,42 @@
-
+import configparser
+from datetime import datetime, timedelta, timezone
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
-# 各種 Google Calendar APIの依存関係 を import できたことを確認
-Request
-InstalledAppFlow
-build
+
+# google calendar event fetcher for poc
+def google_calendar_fetcher():
+    api_version = 'v3'
+    calendar_id = 'ssek01group1kusakaripoc@gmail.com'
+    # read api key
+    config = configparser.ConfigParser()
+    config.read('credentials.ini')
+    api_key = config.get('credentials', 'google_api_key')
+
+    # get events 2 weeks ahead in JST
+    JST = timezone(timedelta(hours=+9))
+    event_start = datetime.now(JST).isoformat()
+    event_end = (datetime.now(JST) + timedelta(weeks=2)).isoformat()
+
+    # event fetch
+    service = build('calendar', api_version, developerKey=api_key)
+    events = service.events().list(
+        calendarId=calendar_id,
+        timeMin=event_start,
+        timeMax=event_end,
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute().get('items', [])
+
+    if not events:
+        print('2週間以内のイベントは見つかりませんでした。')
+    else:
+        print('2週間以内のイベント:')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            end = event['end'].get('dateTime', event['end'].get('date'))
+            summary = event['summary']
+            print(f'{start} - {end}: {summary}')
+
+
+if __name__ == '__main__':
+    google_calendar_fetcher()
